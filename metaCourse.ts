a43f45d52a28e755372c6abc8eb541beeb414cde
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-12 15:33:24 +0200

    defensive programming

--- metaCourse.ts
+++ metaCourse.ts
@@ -16,3 +16,23 @@ for (var i = 0; i < arrOfKeys.length; i++) {
   console.log(drone[arrOfKeys[i]]);
 }
 // 100 200 red
+
+// ------------- Defensive programming
+
+function letterFinder(word, match) {
+  var condition1 = typeof word == "string" && word.length >= 2;
+  var condition2 = typeof match == "string" && match.length == 1;
+  if (condition1 && condition2) {
+    for (i = 0; i < word.length; i++) {
+      if (word[i] == match) {
+        console.log("Found the", match, "at", i);
+      } else {
+        console.log("---No match found at", i);
+      }
+    }
+  } else {
+    console.log("Please pass correct arguments to the function");
+  }
+}
+letterFinder([], []);
+letterFinder("cat", "c");
