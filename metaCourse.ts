f0248eacd967478201cdb200e6a4993a98a54980
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-15 21:59:28 +0200

    using classes in JavaScript

--- metaCourse.ts
+++ metaCourse.ts
@@ -90,3 +90,26 @@ var purchase = {
 };
 
 purchase.totalPrice(); //120
+
+// using classes in JavaScript
+
+class Bird {
+  useWings() {
+    console.log("Flying!");
+  }
+}
+class Eagle extends Bird {
+  useWings() {
+    super.useWings();
+    console.log("Barely flapping!");
+  }
+}
+class Penguin extends Bird {
+  useWings() {
+    console.log("Diving!");
+  }
+}
+var baldEagle = new Eagle();
+var kingPenguin = new Penguin();
+baldEagle.useWings(); // "Flying! Barely flapping!"
+kingPenguin.useWings(); // "Diving!"
