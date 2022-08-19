d30f6f6460647b7e5fea0077df838315738a94b8
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-19 15:49:16 +0200

    first challenge meta course

--- metaCourse.ts
+++ metaCourse.ts
@@ -214,3 +214,51 @@ for (prop of sportsCar) {
   // @ts-ignore
   console.log(prop + ": " + sportsCar[prop]); // speed : fast
 }
+
+// Challenge JavaScript
+
+// Task 1: Code a Person class
+class Person {
+  name: string;
+  age: number;
+  energy: number;
+
+  constructor(name = "Tom", age = 20, energy = 100) {
+    this.name = name;
+    this.age = age;
+    this.energy = energy;
+  }
+  sleep() {
+    return (this.energy += 10);
+  }
+  doSomethingFun() {
+    return (this.energy -= 10);
+  }
+}
+
+// Task 2: Code a Worker classc
+class Worker extends Person {
+  constructor(xp = 0, hourlyWage = 10, name = "Alice", age = 30, energy = 120) {
+    super(name, age, energy);
+    this.xp = xp;
+    this.hourlyWage = hourlyWage;
+  }
+  gotToWork() {
+    return (this.xp += 10);
+  }
+}
+
+// Task 3: Code an intern object, run methods
+function intern() {
+  let work = new Worker(0, 10, "Bob", 21, 110);
+  work.gotToWork();
+  return work;
+}
+console.log(intern());
+
+// Task 4: Code a manager object, methods
+function manager() {
+  let work = new Worker(100, 30, "Alice", 30, 120);
+  work.doSomethingFun();
+  return work;
+}
